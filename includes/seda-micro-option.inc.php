82dec9f4afe12ea32d1640603935ea0a3b93b7a2
mosmith55 <mosmith5655@gmail.com>
2015-12-11 03:37:24 +0000

    adding nonce for WP form as suggested

--- includes/seda-micro-option.inc.php
+++ includes/seda-micro-option.inc.php
@@ -8,7 +8,7 @@ function seda_micro_options() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	$keys= get_option("seda_keys");
-	if (isset($_GET['gen_key']))
+	if (isset($_POST['gen_key']) && check_admin_referer('genkey_action','genkey_nonce')
 	{
 		$seda_keys = new SedaCrypt();
 		update_option("seda_keys", $seda_keys->generate(),0);
@@ -17,10 +17,11 @@ function seda_micro_options() {
 	echo '<p>Here is where the form would go if I actually had options.</p><pre>';
 	echo $keys['public'];
 	echo '</pre>
-<form type=post >
+<form method="post">
 <input type=submit value="Generate new Key" />
 <input type=hidden name=gen_key value=1 />
 <input type=hidden name=page value=seda-micro-id />
+'.wp_nonce_field( 'genkey_action', 'genkey_nonce' ).'
 </form>
 </div>';
 }
